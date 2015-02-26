569b35f01f51c813c6c32e5c66658a49807a2271
Pulkit Jaiswal <pulkitj@icloud.com>
2015-02-26 14:55:04 +0800

    works

--- js/flightlog.js
+++ js/flightlog.js
@@ -1,30 +1,38 @@
 var rowCSV;
 
-d3.csv("csv/test.csv")
+d3.csv("csv/test.csv", init)
     .row(function(d) { return {lat: d.lat, lon: d.lon}; })
-    .get(function(error, rows) { console.log(rows); });
+    .get(function(error, rows) { rowCSV = rows; });
 
 
 //mapbox setup
+function init(){
+  L.mapbox.accessToken = 'pk.eyJ1IjoiZ2FydWRhcm9ib3RpY3MiLCJhIjoiUkdUN3gtOCJ9.F0kNKhYZaEm5jwSyHKpgSw';
+  var map = L.mapbox.map('map', 'examples.map-h67hf2ic')
+    .setView([parseFloat(rowCSV[0].lat),
+    parseFloat(rowCSV[0].lon)], 18);
 
-L.mapbox.accessToken = 'pk.eyJ1IjoiZ2FydWRhcm9ib3RpY3MiLCJhIjoiUkdUN3gtOCJ9.F0kNKhYZaEm5jwSyHKpgSw';
-var map = L.mapbox.map('map', 'examples.map-h67hf2ic')
-  .setView([29, -26], 2);
+  var marker = L.marker([-73, 40], {
+      icon: L.mapbox.marker.icon({
+        'marker-color': '#f86767'
+      })
+  });
 
-var marker = L.marker([-73, 40], {
-    icon: L.mapbox.marker.icon({
-      'marker-color': '#f86767'
-    })
-});
+  var t = 0;
+  window.setInterval(function() {
+      // Making a lissajous curve just for fun.
+      // Create your own animated path here.
+      //console.log(rowCSV[t].lat);
+      console.log(parseFloat(rowCSV[t].lat),
+      parseFloat(rowCSV[t].lon));
+      marker.setLatLng(L.latLng(
+          parseFloat(rowCSV[t].lat),
+          parseFloat(rowCSV[t].lon)));
+      t += 1;
+  }, 50);
 
-var t = 0;
-window.setInterval(function() {
-    // Making a lissajous curve just for fun.
-    // Create your own animated path here.
-    marker.setLatLng(L.latLng(
-        Math.cos(t * 0.5) * 50,
-        Math.sin(t) * 50));
-    t += 0.1;
-}, 50);
+  marker.addTo(map);
 
-marker.addTo(map);
+}
+
+setTimeout(init, 1000);
